9d886639e1b8b71a2b1a9d77afc0fc713f080688
Daniil Jave <javeoff@yandex.ru>
2022-11-06 22:29:50 +0300

    feat: add useSetState test

--- tests/useSetState.test.ts
+++ tests/useSetState.test.ts
@@ -1,22 +1,73 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 import { useSetState } from '../src';
 
-test('Empty object hook', () => {
-  const { result } = renderHook(() => {
-    const [state, setState] = useSetState({});
+const initializeHook = (initialState: Record<string, unknown> = {}) => {
+  return renderHook(() => {
+    const [state, setState] = useSetState(initialState);
 
     return {
       state,
       setState,
     }
   })
+}
 
-  const {
-    state, setState
-  } = result.current;
+const useHook = (initialState: Record<string, unknown> = {}) => {
+  const { result } = initializeHook(initialState);
+  return result.current;
+}
 
-  type TState = Record<string, unknown>;
+describe('useSetState', () => {
+  test('should be empty object state', () => {
+    const { state, setState } = useHook()
 
-  expect(typeof state).toBe('object');
-  expect(Object.values(state as TState).length).toBe(0);
+    type TState = Record<string, unknown>;
+
+    expect(typeof state).toBe('object');
+    expect(Object.values(state as TState).length).toBe(0);
+  })
+
+  test('should init state and setter', () => {
+    const { state, setState } = useHook({
+      x: 2,
+    })
+
+    expect(state).toEqual({ x: 2 })
+    expect(setState).toBeInstanceOf(Function);
+  })
+
+  test('should update state', () => {
+    const { state, setState } = useHook({
+      x: 2,
+    })
+
+    act(() => setState({ x: 3 }))
+
+    expect(state).toEqual({ x: 3 })
+  })
+
+  test('should expand state', () => {
+    const { state, setState } = useHook({
+      x: 2,
+    })
+
+    act(() => setState({ y: 3 }))
+
+    expect(state).toEqual({ x: 2, y: 3 })
+  })
+
+  test('should return memoized setState', () => {
+    const { result, rerender } = initializeHook({
+      flag: true,
+    })
+    const { setState: setStateBefore } = result.current;
+
+    act(() => setStateBefore({
+      flag: false,
+    }))
+    rerender()
+    const { setState: setStateAfter } = result.current;
+
+    expect(setStateBefore).toEqual(setStateBefore)
+  })
 })
